d098cb412b2455c21a6da7cc1d94c860ce871d4d
Aleksandr Omelenetskiy <aleksandr_omelenetskiy@epbybrew0056.minsk.epam.com>
2022-04-14 16:03:39 +0300

    Telegram update function stub

--- bot.js
+++ bot.js
@@ -1,29 +1,9 @@
 require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const { BOT_TOKEN } = process.env;
 
-const TelegramBot = require('node-telegram-bot-api');
-const { BOT_CHAT_ID, BOT_TOKEN } = process.env;
+const bot = new Telegraf(BOT_TOKEN);
 
-console.log(BOT_CHAT_ID);
-console.log(BOT_TOKEN);
-console.log(process.env);
-const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+bot.start((ctx) => ctx.reply('Welcome'));
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  console.log(chatId);
-  bot.sendMessage(chatId, resp);
-});
-
-bot.onText(/\/start/, (msg, match) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, 'Hi pidr');
-});
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  if (msg.text == 'dog') {
-    bot.sendMessage(chatId, "You sent 'dog'");
-  }
-});
+bot.launch();
