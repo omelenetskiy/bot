b03923b85a3dd13a9557af7f1292598366cf9454
omelenetskiy <omelenetskiy@google.com>
2022-04-14 16:24:32 +0300

    Respond to messages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,3 +1,11 @@
+require('dotenv').config();
+const axios = require('axios').default;
+
+await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+  chat_id: JSON.parse(event.body).message.chat.id,
+  text: 'I got your message!',
+});
+
 exports.handler = async (event) => {
   console.log('Received an update from Telegram!', event.body);
   return { statusCode: 200 };
