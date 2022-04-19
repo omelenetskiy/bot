85fa6576fd19fe89c5aea01c3c3a416977306f24
Yauheni Rohau <yrohau@inspirebrands.com>
2022-04-19 14:56:55 +0300

    fix

--- netlify/functions/initializeBot.js
+++ netlify/functions/initializeBot.js
@@ -1,10 +1,10 @@
-const { writeUserData } = require('./firebase');
+// const { writeUserData } = require('./firebase');
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => {
   const { id, is_bot: isBot, first_name: firstName, last_name: lastName } = ctx.from;
-  writeUserData(id, firstName, lastName);
+  // writeUserData(id, firstName, lastName);
   return ctx.reply(`Welcome, ${id}: ${firstName} ${lastName}. You are ${isBot ? '' : 'not a'} bot`);
 });
 bot.help((ctx) => ctx.reply('Send me a sticker'));
@@ -21,6 +21,6 @@ process.once('SIGTERM', () => bot.stop('SIGTERM'))
 //curl -F "url=https://bot-0071.netlify.app/.netlify/functions/update" https://api.telegram.org/bot5321221901:AAE9oBfGqYxtozpi7WcNf0HqYDUm05XPoBU/setWebhook?url=https://bot-0071.netlify.app/.netlify/functions/update
 
 exports.handler = () => {
-  console.log(`Telegram bot successfully initialized ${process.env.BOT_TOKEN}`);
+  console.log(`Telegram bot successfully initialized`);
   return { statusCode: 200 };
 };
