4df49035c4e3d15930dbb081de15f83b436b0588
omelenetskiy <omelenetskiy@google.com>
2022-04-19 23:40:48 +0300

    make it better

---
+++ bot.js
@@ -0,0 +1,19 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN, { polling: true });
+
+bot.start((ctx) => {
+  const { id, is_bot: isBot, first_name: firstName, last_name: lastName } = ctx.from;
+  // writeUserData(id, firstName, lastName);
+  return ctx.reply(`Welcome, ${id}: ${firstName} ${lastName}. You are ${isBot ? '' : 'not a'} bot`);
+});
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'));
+bot.catch((e) => {
+  console.log('ERROR!!!', e);
+});
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
